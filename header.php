b5a3b2f259e0c5288f5a24280ec8c7695ae0fef1
Yashika Pawar <iamyashikapawar@gmail.com>
2025-01-18 14:08:41 +0530

    Header Modified

--- header.php
+++ header.php
@@ -5,23 +5,21 @@
       <a href="index.html" class="logo d-flex align-items-center">
         <!-- Uncomment the line below if you also wish to use an image logo -->
         <!-- <img src="assets/img/logo.png" alt=""> -->
-        <h1 class="sitename">Selecao</h1>
+        <h1 class="sitename">Algo Therapist</h1>
       </a>
 
       <nav id="navmenu" class="navmenu">
         <ul>
-          <li><a href="#hero" class="active">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#portfolio">Portfolio</a></li>
-          <li><a href="#team">Team</a></li>
+          <li><a href="#about">Our Offerings</a></li>
+          <li><a href="#services">AI-Powered Therapist</a></li>
+          <li><a href="#portfolio">Goal Tracker</a></li>
+          <li><a href="#team">Reminders</a></li>
+          <li><a href="#team">Mindfulness</a></li>
           <?php if (isset($_SESSION['user_logged_in']) && $_SESSION['user_logged_in'] === true) { ?>
             <li><a href="/algo-alchemists/logout.php"><?php echo $_SESSION['name']; ?></a></li>
           <?php } else {?>
-            <li><a href="/algo-alchemists/forms/otpLogin.php">Login</a></li>
+            <li><a href="/algo-alchemists/forms/otpLogin.php" class="active">Login</a></li>
           <?php } ?>
-          <li><a href="blog.html">Blog</a></li>
-          <li><a href="#contact">Contact</a></li>
         </ul>
         <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
       </nav>
